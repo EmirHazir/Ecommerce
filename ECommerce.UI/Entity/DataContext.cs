c97d6091b2a775a4395e36066501e0eab0534507
EmirHazir <emircanaziz@gmail.com>
2017-12-31 04:59:05 +0300

    Re-Created
    Created new ui

---
+++ ECommerce.UI/Entity/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.UI.Entity
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DataConnection")
+        {
+            Database.SetInitializer(new DataInitializer());
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+    }
+}
